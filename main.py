0d5dae038d2898b7823928d74ba9fd7c9e0a0adc
Žilvinas <z.aleksa@student.tue.nl>
2025-07-03 18:49:19 +0300

    first try

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+# main.py
 from typing import Union
-
 from fastapi import FastAPI
+from mangum import Mangum # Add this import
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
@@ -12,3 +12,6 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+# This is the handler function for AWS Lambda
+handler = Mangum(app)
