65a4c3a87c32238014ad1256d4afd807ee306b77
Žilvinas <z.aleksa@student.tue.nl>
2025-07-05 12:51:31 +0300

    changing the api strucutre

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-# main.py
-from typing import Union
-from fastapi import FastAPI
-from mangum import Mangum # Add this import
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from mangum import Mangum
 
 app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-# This is the handler function for AWS Lambda
 handler = Mangum(app)
+
+class SurveyData(BaseModel):
+    shop_domain: str
+    customer_email: str
+    responses: dict  # or more specific schema later
+
+@app.post("/survey")
+async def handle_survey(data: SurveyData):
+    print("Survey received:", data)
+    return {"status": "ok", "message": "Survey received"}
