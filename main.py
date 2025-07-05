6ece656227550a37a9aed87b31c5c111a61d9ac5
Žilvinas <z.aleksa@student.tue.nl>
2025-07-05 13:30:36 +0300

    survey

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from mangum import Mangum
+from starlette.middleware import Middleware
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+middleware = [
+    Middleware(
+        # type: ignore
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+]
+
+app = FastAPI(middleware=middleware)
 handler = Mangum(app)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 class BookSurvey(BaseModel):
     character: str
     theme: str
     product_id: str
+
 
 @app.post("/survey")
 async def handle_survey(data: BookSurvey):
